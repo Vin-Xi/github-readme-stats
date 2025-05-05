db1f6b9b967a94255cba1ae08b5eb4de4d2326bb
Vin-Xi <45799230+Vin-Xi@users.noreply.github.com>
2025-05-05 15:04:18 +0400

    Update calculateRank.js

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -80,7 +80,7 @@ function calculateRank({
 
   const level = LEVELS[THRESHOLDS.findIndex((t) => rank * 100 <= t)];
 
-  return { "B+", percentile: rank * 100 };
+  return { level:"B+", percentile: rank * 100 };
 }
 
 export { calculateRank };
